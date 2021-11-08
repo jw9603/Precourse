4ecb20e988c58dd08870547af3af74c05bcece00
jiwon <jwjw9603@naver.com>
2021-11-08 14:00:27 +0900

    노력하자

---
+++ GAME/__main__.py
@@ -0,0 +1,5 @@
+from sound import echo
+ 
+if __name__ == '__main__':
+    print('Hello Game')
+    print(echo.echo_play(10))
---
+++ GAME/sound/echo.py
@@ -0,0 +1,3 @@
+def echo_play(echo_number):
+    print('echo {} number start'.format(echo_number))
+    return 'hi'
